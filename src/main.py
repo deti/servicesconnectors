1a1dd38e9d112f37b90f630f6f46dd8dc5a43a57
Denis Timofeev <detijazzz@gmail.com>
2024-01-31 11:43:32 +0400

    Rename connector -> connection in main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,12 @@
-""" Main for connector application. """
+""" Main for connections application. """
 
 from fastapi import FastAPI
 
-from src.connections.api import router as connectors_router
+from src.connections.api import router as connections_router
 
 app = FastAPI()
 
-app.include_router(connectors_router)
+app.include_router(connections_router)
 
 
 @app.get("/")
