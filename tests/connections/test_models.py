dd4859e73891423667b9174b60982244e29df6e9
Denis Timofeev <detijazzz@gmail.com>
2024-01-31 12:00:54 +0400

    Rename connector -> connection /tests/connections/api/test_appstore.py

--- tests/connections/test_models.py
+++ tests/connections/test_models.py
@@ -14,18 +14,18 @@ from tests.connections.fakes import fake_appstore_settings, fake_connection
 fake = Faker()
 
 
-def test_get_connector_returns_none_when_non_exsited(db: Session):
-    connector = get_connection(db, "test-uuid")
-    assert connector is None
+def test_get_connection_returns_none_when_non_exsited(db: Session):
+    connection = get_connection(db, "test-uuid")
+    assert connection is None
 
 
-def test_get_connector_returns_connector_when_exsited(db: Session):
-    connector = fake_connection()
-    db.add(connector)
+def test_get_connection_returns_connection_when_exsited(db: Session):
+    connection = fake_connection()
+    db.add(connection)
     db.commit()
-    db.refresh(connector)
+    db.refresh(connection)
 
-    assert get_connection(db, str(connector.uuid)) == connector
+    assert get_connection(db, str(connection.uuid)) == connection
 
 
 def fake_connection_create() -> ConnectionCreate:
@@ -36,54 +36,54 @@ def fake_connection_create() -> ConnectionCreate:
     )
 
 
-def test_create_connector_creates_connector(db: Session):
-    connector_create = fake_connection_create()
+def test_create_connection_creates_connection(db: Session):
+    connection_create = fake_connection_create()
 
-    connector = create_connection(db, connector_create)
+    connection = create_connection(db, connection_create)
 
-    assert connector.type == connector_create.type
-    assert connector.settings == json.dumps(connector_create.settings)
-    assert connector.description == connector_create.description
+    assert connection.type == connection_create.type
+    assert connection.settings == json.dumps(connection_create.settings)
+    assert connection.description == connection_create.description
 
 
-def test_or_create_connector_creates_connector_when_not_exsited(db: Session):
-    connector_create = fake_connection_create()
+def test_or_create_connection_creates_connection_when_not_exsited(db: Session):
+    connection_create = fake_connection_create()
 
     # No Connect before test
-    expected_uuid = generate_uuid_from_dict(connector_create.settings)
+    expected_uuid = generate_uuid_from_dict(connection_create.settings)
     assert get_connection(db, expected_uuid) is None
 
-    # Connector created
-    connector = get_or_create_connection(db, connector_create)
-    assert expected_uuid == connector.uuid
-    assert connector.type == connector_create.type
-    assert connector.settings == json.dumps(connector_create.settings)
-    assert connector.description == connector_create.description
+    # connection created
+    connection = get_or_create_connection(db, connection_create)
+    assert expected_uuid == connection.uuid
+    assert connection.type == connection_create.type
+    assert connection.settings == json.dumps(connection_create.settings)
+    assert connection.description == connection_create.description
 
 
-def test_or_create_connector_returns_connector_when_exsited(db: Session):
-    connector_create = fake_connection_create()
-    created_item = create_connection(db, connector_create)
-    retrieved_item = get_or_create_connection(db, connector_create)
+def test_or_create_connection_returns_connection_when_exsited(db: Session):
+    connection_create = fake_connection_create()
+    created_item = create_connection(db, connection_create)
+    retrieved_item = get_or_create_connection(db, connection_create)
 
     assert retrieved_item == created_item
 
 
-def test_all_connectors_uuids_returns_empty_list_when_no_connector(db: Session):
+def test_all_connections_uuids_returns_empty_list_when_no_connection(db: Session):
     assert get_all_connections(db) == []
 
 
-def test_get_all_connectors_returns_all_connectrs(db: Session):
+def test_get_all_connections_returns_all_connectrs(db: Session):
     expected_uuids = [
         create_connection(db, fake_connection_create()) for _ in range(10)
     ]
     assert set(get_all_connections(db)) == set(expected_uuids)
 
 
-def test_delete_connector_from_db_deletes_connector_when_exsited(db: Session):
-    connector = create_connection(db, fake_connection_create())
-    connector_uuid = str(connector.uuid)  # avoid mypy error
-    assert get_connection(db, connector_uuid) is not None
+def test_delete_connection_from_db_deletes_connection_when_exsited(db: Session):
+    connection = create_connection(db, fake_connection_create())
+    connection_uuid = str(connection.uuid)  # avoid mypy error
+    assert get_connection(db, connection_uuid) is not None
 
-    delete_connection_from_db(db, connector_uuid)
-    assert get_connection(db, connector_uuid) is None
+    delete_connection_from_db(db, connection_uuid)
+    assert get_connection(db, connection_uuid) is None
